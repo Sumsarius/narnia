696bc5f20774c02c25b4e43c024acca51f370ad2
Rasmus Møller <rasmus@sybogames.com>
2024-12-19 14:12:43 +0100

    Personal learning Unity basics

---
+++ Assets/Scripts/PrintToConsole.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I'm faster than the speed of light vrooom");
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
