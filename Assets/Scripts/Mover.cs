505f63f041fec51096a579ff830be0cab9234ef1
Rasmus <rasmus@sybogames.com>
2024-12-20 15:45:07 +0100

    obstacle course

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -9,15 +9,26 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
- 
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
-      float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-      float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with W,S,A,D or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
       
-      transform.Translate(xValue,0,zValue);      
+        transform.Translate(xValue,0,zValue);   
     }
 }
