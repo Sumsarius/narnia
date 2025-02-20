9d98508425c0a42769949bf43401629655d6c1d1
Rasmus <rasmus@sybogames.com>
2025-02-20 17:34:30 +0100

    Admiral, MusicPlayer, Enemy Waves

--- Assets/Scripts/Lecture4/Enemy.cs
+++ Assets/Scripts/Lecture4/Enemy.cs
@@ -4,15 +4,16 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] GameObject hitVFX;
-    [SerializeField] private Transform parent;
     [SerializeField] int scorePerHit = 15;
     [SerializeField] private int hitPoints = 10;
     
     ScoreBoard scoreBoard;
+    GameObject parentGameObject;
 
     void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
         Rigidbody  rb = gameObject.AddComponent<Rigidbody>();
         rb.useGravity = false;
     }
@@ -28,14 +29,14 @@ public class Enemy : MonoBehaviour
     private void ProcessHit()
     {
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         hitPoints--;
-        scoreBoard.IncreaseScore(scorePerHit);
     }
     private void KillEnemy()
     {
+        scoreBoard.IncreaseScore(scorePerHit);
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
     }
 
---
+++ Assets/Scripts/Lecture4/MusicPlayer.cs
@@ -0,0 +1,20 @@
+
+
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+   void Awake()
+   {
+      int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+      if (numMusicPlayers > 1)
+      {
+         Destroy(gameObject);
+      }
+      else
+      {
+         DontDestroyOnLoad(gameObject);
+      }
+   }
+   
+}
